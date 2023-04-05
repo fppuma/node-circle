7ef0969df8c3be30db30efb0fd9c907b1d81a8b6
Francisco Puma <fppuma@gmail.com>
2023-04-05 08:04:20 -0500

    update endpoints

--- endpoints/users/index.js
+++ endpoints/users/index.js
@@ -1,14 +1,25 @@
-const axios = require('axios')
-
 //handler is a function that returns an object
 const handlers = ({axios}) => ({
   get: async (req, res) => {
     const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
     res.status(200).send(data)
   },
-  post: () =>{},
-  put: () =>{},
-  delete: () =>{},
+  post: async (req, res) => {
+    const {body} = req
+    const {data} = await axios.post('https://jsonplaceholder.typicode.com/users', body)
+    res.status(201).send(data)
+  },
+  put: async (req, res) => {
+    const {id} = req.params
+    const {body} = req
+    await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, body)
+    res.sendStatus(204)
+  },
+  delete: async (req, res) => {
+    const {id} = req.params
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+    res.sendStatus(204)
+  },
 })
 
 module.exports = handlers
--- index.js
+++ index.js
@@ -15,24 +15,11 @@ app.use(bodyParser.json())
 const usersHandler = users({axios})
 app.get('/', usersHandler.get)
 
-app.post('/', async (req, res) => {
-  const {body} = req
-  const {data} = await axios.post('https://jsonplaceholder.typicode.com/users', body)
-  res.status(201).send(data)
-})
+app.post('/', usersHandler.post)
 
-app.put('/:id', async (req, res) => {
-  const {id} = req.params
-  const {body} = req
-  await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, body)
-  res.sendStatus(204)
-})
+app.put('/:id', usersHandler.put)
 
-app.delete('/:id', async (req, res) => {
-  const {id} = req.params
-  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-  res.sendStatus(204)
-})
+app.delete('/:id', usersHandler.delete)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
