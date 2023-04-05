93670894af23d76f81b0893f2d6e41b0b28f3dda
Francisco Puma <fppuma@gmail.com>
2023-04-04 21:42:14 -0500

    get with axios

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
+  res.send(data)
 })
 
 app.listen(port, () => {
